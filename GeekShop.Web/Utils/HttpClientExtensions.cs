5502376275b76cd8798acab4fdb11d1385513fba
Mattheus Germano <78939773+mattheusmgermano@users.noreply.github.com>
2022-05-23 18:22:13 +0100

    Implemented Put and Post operations in HttpClientExtensions.cs

--- GeekShop.Web/Utils/HttpClientExtensions.cs
+++ GeekShop.Web/Utils/HttpClientExtensions.cs
@@ -1,9 +1,12 @@
-﻿using System.Text.Json;
+﻿using System.Net.Http.Headers;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc.Formatters;
 
 namespace GeekShop.Web.Utils;
 
 public static class HttpClientExtensions
 {
+    private static MediaTypeHeaderValue _contentType = new MediaTypeHeaderValue("application/json");
     public static async Task<T?> ReadContentAsync<T>(this HttpResponseMessage response)
     {
         if (!response.IsSuccessStatusCode)
@@ -14,5 +17,26 @@ public static class HttpClientExtensions
         {
             PropertyNameCaseInsensitive = true
         });
+    }
+
+    public static Task<HttpResponseMessage> PostAsJson<T>(
+        this HttpClient httpClient,
+        string url,
+        T data)
+    {
+        var dataAsString = JsonSerializer.Serialize(data);
+        var content = new StringContent(dataAsString);
+        content.Headers.ContentType = _contentType;
+        return httpClient.PostAsync(url, content);
+        
+    }public static Task<HttpResponseMessage> PutAsJson<T>(
+        this HttpClient httpClient,
+        string url,
+        T data)
+    {
+        var dataAsString = JsonSerializer.Serialize(data);
+        var content = new StringContent(dataAsString);
+        content.Headers.ContentType = _contentType;
+        return httpClient.PutAsync(url, content);
     }
 }
