779f17b3492a6afadf362b420cc7c1b27528bfe6
Mattheus Germano <78939773+mattheusmgermano@users.noreply.github.com>
2022-05-23 18:14:46 +0100

    Created HttpClientExtensions to serve as PostAsJsonAsync. Implemented ReadAs.

---
+++ GeekShop.Web/Services/IServices/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using GeekShop.Web.Models;
+
+namespace GeekShop.Web.Services.IServices;
+
+public interface IProductService
+{
+    Task<IEnumerable<ProductModel>> FindAllProducts();
+    Task<ProductModel> FindProductById(long id);
+    Task<ProductModel> CreateProduct(ProductModel product);
+    Task<ProductModel> UpdateProduct(ProductModel product);
+    Task<bool> DeleteProductById(long id);
+}
---
+++ GeekShop.Web/Utils/HttpClientExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+
+namespace GeekShop.Web.Utils;
+
+public static class HttpClientExtensions
+{
+    public static async Task<T?> ReadContentAsync<T>(this HttpResponseMessage response)
+    {
+        if (!response.IsSuccessStatusCode)
+            throw new ApplicationException($"Oopsie! Something went wrong! [{response.ReasonPhrase}]");
+        
+        var dataAsString = await response.Content.ReadAsStreamAsync().ConfigureAwait(false);
+        return JsonSerializer.Deserialize<T>(dataAsString, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        });
+    }
+}
